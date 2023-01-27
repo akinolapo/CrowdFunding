4f1f85847eb951935e8e3ca51d31912b36245cc7
akinolapo <akinolapo@gmail.com>
2023-01-27 18:17:33 +0100

    donateToCampaing function

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -23,9 +23,34 @@ contract CrowdFunding {
 
         // is everything okay?
         require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaings++;
+
+        return numberOfCampaings - 1;
     }
 
-    function donateToCampaing() {}
+    function donateToCampaing(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
 
     function getDonators() {}
 
