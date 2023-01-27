25d4824f31325ef57e1225bb088452888ec3e573
akinolapo <akinolapo@gmail.com>
2023-01-27 14:57:55 +0100

    createCampaing function

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -2,5 +2,32 @@
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
-    constructor() {}
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaings = 0;
+
+    function createCampaing(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns(uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaings];
+
+        // is everything okay?
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+    }
+
+    function donateToCampaing() {}
+
+    function getDonators() {}
+
+    function getCampaigns() {}
 }
